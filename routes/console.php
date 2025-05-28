b038a0e15f8afe384f55155c7245506ba1863128
Daniel Henninger <dh@passolution.de>
2025-05-28 22:29:37 +0200

    Automatisierung

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Bus\DispatchesCommands;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('api:monitor')->everyMinute();
